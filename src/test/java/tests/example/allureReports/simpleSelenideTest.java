13b20d5302d654f8a0bc1808f960a13ea43f4c0e
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-03-16 18:44:27 +0300

    Add simple tests with Allure Report

---
+++ src/test/java/tests/example/allureReports/simpleSelenideTest.java
@@ -0,0 +1,31 @@
+package tests.example.allureReports;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
+
+
+public class simpleSelenideTest {
+
+    @Test
+    public void simpleIssueTest() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com");
+
+        $(byText("Search or jump to...")).click();
+        $("#query-builder-test").sendKeys("eroshenkoam/allure-example");
+        $("#query-builder-test").submit();
+
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issue-tab").click();
+        $(withText("#80")).should(Condition.exist);
+    }
+}
