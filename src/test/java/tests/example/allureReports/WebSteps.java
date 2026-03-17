0f9f36f350d4f6725208202d87bcf5e3c0c6d412
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-03-17 19:44:28 +0300

    Add attachments example

--- src/test/java/tests/example/allureReports/WebSteps.java
+++ src/test/java/tests/example/allureReports/WebSteps.java
@@ -1,7 +1,12 @@
 package tests.example.allureReports;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.WebDriverRunner;
+import com.codeborne.selenide.commands.TakeScreenshot;
+import io.qameta.allure.Attachment;
 import io.qameta.allure.Step;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selectors.withText;
@@ -36,6 +41,11 @@ public class WebSteps {
     @Step ("Проверяем наличие Issue с номером {issue}")
     public void shouldSeeIssueWithNumber(int issue) {
         $(withText("#" + issue)).should(Condition.exist);
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png", fileExtension = "png")
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
 }
