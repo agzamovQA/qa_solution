6678ec3f2781cf52adc0ffe33c3ef5544893f15f
Agzamov Vladimir <agzamov.qa@yandex.ru>
2026-03-17 18:26:46 +0300

    Add testLambdaStep example

---
+++ src/test/java/tests/example/allureReports/simpleSelenideTestWithStep.java
@@ -0,0 +1,48 @@
+package tests.example.allureReports;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Step;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+
+public class simpleSelenideTestWithStep {
+
+    private static final String REPOSITORY = "eroshenkoam/allure-example";
+    private static final int ISSUE = 80;
+
+    @Test
+    public void testLambdaStep() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открываем главную страницу", () -> { // Лямбда тест - тест из одного аргумента (метода)
+            open("https://github.com");
+        });
+
+        step("Ищем репозиторий " + REPOSITORY, () -> {
+            $(byText("Search or jump to...")).click();
+            $("#query-builder-test").sendKeys(REPOSITORY);
+            $("#query-builder-test").submit();
+        });
+
+        step("Кликаем по ссылке репозитория " + REPOSITORY, () -> {
+            $(linkText(REPOSITORY)).click();
+        });
+
+        step("Открываем таб Issues", () -> {
+            $("#issue-tab").click();
+        });
+
+        step("Проверяем наличие Issue с номером " + ISSUE, () -> {
+            $(withText("#" + ISSUE)).should(Condition.exist);
+        });
+    }
+}
